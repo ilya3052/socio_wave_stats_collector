2148ac7334efc08da895a56f1546094306c01b4f
ilya3052 <ipopadin5@gmail.com>
2026-04-03 13:07:52 +0300

    отправка статистики в базу данных через цикл

--- src/stats/send.py
+++ src/stats/send.py
@@ -6,61 +6,62 @@ from src.models import AbsoluteStatsSchema, SnapshotSchemaCreate, SnapshotModel,
 from src.repositories import AbsoluteStatsRepository, SnapshotRepository, SnapshotStatsRepository
 
 
-async def send_daily_stats_to_db(stats):
-    try:
-        with Session() as session:
-            group_id = stats.get('External ID')
-            abs_repo = AbsoluteStatsRepository(session)
-            abs_stats_instance = abs_repo.get_by_group(group_id)
-            abs_stats_schema = AbsoluteStatsSchema.model_validate(abs_stats_instance)
-            participants_count = abs_stats_schema.participants_count
+async def send_daily_stats_to_db(stats, snapshot_type):
+    for stats_elem in stats:
+        try:
+            with Session() as session:
+                group_id = stats_elem.get('External ID')
+                abs_repo = AbsoluteStatsRepository(session)
+                abs_stats_instance = abs_repo.get_by_group(group_id)
+                abs_stats_schema = AbsoluteStatsSchema.model_validate(abs_stats_instance)
+                participants_count = abs_stats_schema.participants_count
 
-            snapshot_repo = SnapshotRepository(session)
-            snapshot_schema = SnapshotSchemaCreate.model_validate({
-                "last_message_id": stats.get('ID последней записи'),
-                "type": 1,
-                "group_id": stats.get('Internal ID')
-            })
-            snapshot_instance = SnapshotModel(**snapshot_schema.model_dump())
+                snapshot_repo = SnapshotRepository(session)
+                snapshot_schema = SnapshotSchemaCreate.model_validate({
+                    "type": snapshot_type,
+                    "group_id": stats_elem.get('Internal ID')
+                })
+                snapshot_instance = SnapshotModel(**snapshot_schema.model_dump())
 
-            snapshot_id = snapshot_repo.add(snapshot_instance)
+                snapshot_id = snapshot_repo.add(snapshot_instance)
 
-            snapshot_stats_repo = SnapshotStatsRepository(session)
-            snapshot_stats_schema = SnapshotStatsSchemaCreate.model_validate({
-                "repost_count": stats.get('Репосты', 0),
-                "likes_count": stats.get('Лайки', 0),
-                "views_count": stats.get('Просмотры', 0),
-                "participants_delta": participants_count - stats.get('Подписчики', 0),
-                "comms_count": stats.get('Комментарии', 0),
-                "snapshot_id": snapshot_id,
-                "coverage": 1
-            })
-            snapshot_stats_repo.add(SnapshotStatsModel(**snapshot_stats_schema.model_dump()))
+                snapshot_stats_repo = SnapshotStatsRepository(session)
+                snapshot_stats_schema = SnapshotStatsSchemaCreate.model_validate({
+                    "repost_count": stats_elem.get('Репосты', 0),
+                    "likes_count": stats_elem.get('Лайки', 0),
+                    "views_count": stats_elem.get('Просмотры', 0),
+                    "participants_delta": participants_count - stats_elem.get('Подписчики', 0),
+                    "comms_count": stats_elem.get('Комментарии', 0),
+                    "snapshot_id": snapshot_id,
+                    "coverage": 1
+                })
+                snapshot_stats_repo.add(SnapshotStatsModel(**snapshot_stats_schema.model_dump()))
 
-            snapshot_repo.commit()
-            snapshot_stats_repo.commit()
-        return True
-    except ValidationError as VE:
-        print(VE)
-        return False
+                snapshot_repo.commit()
+                snapshot_stats_repo.commit()
+        except ValidationError as VE:
+            print(VE)
+            return False
+    return True
 
 
 async def send_absolute_stats_to_db(stats):
-    try:
-        with Session() as session:
-            absolute_stats_repo = AbsoluteStatsRepository(session)
-            absolute_stats_schema = AbsoluteStatsSchemaCreate.model_validate({
-                "likes_count": stats.get('Лайки'),
-                "views_count": stats.get('Просмотры'),
-                "participants_count": stats.get('Подписчики'),
-                "repost_count": stats.get('Репосты'),
-                "comms_count": stats.get('Комментарии'),
-                "group_id": stats.get('Internal ID')
-            })
-            absolute_stats_instance = AbsoluteStatsModel(**absolute_stats_schema.model_dump())
-            absolute_stats_repo.add(absolute_stats_instance)
-            absolute_stats_repo.commit()
-        return True
-    except ValidationError as VE:
-        print(VE)
-        return False
+    for stats_elem in stats:
+        try:
+            with Session() as session:
+                absolute_stats_repo = AbsoluteStatsRepository(session)
+                absolute_stats_schema = AbsoluteStatsSchemaCreate.model_validate({
+                    "likes_count": stats_elem.get('Лайки'),
+                    "views_count": stats_elem.get('Просмотры'),
+                    "participants_count": stats_elem.get('Подписчики'),
+                    "repost_count": stats_elem.get('Репосты'),
+                    "comms_count": stats_elem.get('Комментарии'),
+                    "group_id": stats_elem.get('Internal ID')
+                })
+                absolute_stats_instance = AbsoluteStatsModel(**absolute_stats_schema.model_dump())
+                absolute_stats_repo.add(absolute_stats_instance)
+                absolute_stats_repo.commit()
+        except ValidationError as VE:
+            print(VE)
+            return False
+    return True
