a23252a0f24af11379e801d627c660aac49c1a57
ilya3052 <ipopadin5@gmail.com>
2026-04-03 13:07:17 +0300

    добавление обработки hourly-снапшотов

--- src/stats/handle.py
+++ src/stats/handle.py
@@ -4,8 +4,8 @@ from .send import send_daily_stats_to_db, send_absolute_stats_to_db
 
 async def handle_stats(stats, stats_type):
     match stats_type:
-        case Type.DAILY:
-            if not await send_daily_stats_to_db(stats):
+        case Type.DAILY | Type.HOURLY:
+            if not await send_daily_stats_to_db(stats, stats_type):
                 return False
         case Type.ABSOLUTE:
             if not await send_absolute_stats_to_db(stats):
