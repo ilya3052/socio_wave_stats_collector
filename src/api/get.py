439f5db388e98ab74fb8322c97f63c80d4e6bfb7
ilya3052 <ipopadin5@gmail.com>
2026-04-06 15:50:49 +0300

    создание обертки над функциями получения api платформ

---
+++ src/api/get.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from telethon import TelegramClient
+from vk_api.vk_api import VkApiMethod
+
+from src.api.apiTG import get_tg_api_session
+from src.api.apiVK import get_vk_api_session
+from src.core import Platforms
+from src.models import ServiceAccountDataModel
+
+
+async def get_api(account_data: ServiceAccountDataModel, platform):
+    api: Optional[VkApiMethod | TelegramClient] = None
+    if platform == Platforms.TG:
+        api = get_tg_api_session(account_data.session_path)
+    elif platform == Platforms.VK:
+        api = get_vk_api_session(account_data.service_key)
+    return api
