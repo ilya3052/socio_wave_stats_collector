e735ed18743c9b372502a7536e9d16ad5f84e49f
ilya3052 <ipopadin5@gmail.com>
2026-04-06 22:23:55 +0300

    улучшение обработки ошибок, проверка на корректность значений и re-raise исключений выше

--- src/tasks/create.py
+++ src/tasks/create.py
@@ -1,9 +1,11 @@
 import asyncio
 
 from _asyncio import Task
+from pydantic import ValidationError
 
 from src.api import get_api
 from src.core import Platforms
+from src.exceptions.exceptions import GroupsNotFoundError, GroupHandleError
 from src.models import ServiceAccountModel
 from src.stats import handle_stats
 from src.stats.collect import collect_stats
@@ -14,6 +16,9 @@ async def create_processing_tasks(accounts, **kwargs):
         tasks: list[Task] = []
         for idx, account in enumerate(accounts):  # type: int, ServiceAccountModel
             groups = account.groups
+            if not groups:
+                raise GroupsNotFoundError('Произошла ошибка при получении групп для сервисного аккаунта')
+
             platform: Platforms = kwargs.get('platform')
             api = await get_api(account.data, platform)
 
@@ -27,15 +32,21 @@ async def create_processing_tasks(accounts, **kwargs):
         return tasks
     except ValueError:
         raise
+    except GroupsNotFoundError:
+        raise
+    except GroupHandleError:
+        raise
 
 
 async def create_sending_tasks(stats_results, stats_type):
-    tasks: list[Task] = []
-    print(stats_results)
-    for account in stats_results:
-        for group_stats in account:
-            tasks.append(asyncio.create_task(
-                handle_stats(group_stats, stats_type)
-            ))
+    try:
+        tasks: list[Task] = []
+        for account in stats_results:
+            for group_stats in account:
+                tasks.append(asyncio.create_task(
+                    handle_stats(group_stats, stats_type)
+                ))
 
-    return tasks
+        return tasks
+    except ValidationError:
+        raise
