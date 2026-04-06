4e87087d42388750ecb9a3b7e62ed5d950be1ef0
ilya3052 <ipopadin5@gmail.com>
2026-04-06 15:58:35 +0300

    создание функций по созданию задач

---
+++ src/tasks/create.py
@@ -0,0 +1,40 @@
+import asyncio
+
+from _asyncio import Task
+
+from src.api import get_api
+from src.core import Platforms
+from src.models import ServiceAccountModel
+from src.stats import handle_stats
+from src.stats.collect import collect_stats
+
+
+async def create_processing_tasks(accounts, **kwargs):
+    try:
+        tasks: list[Task] = []
+        for idx, account in enumerate(accounts):  # type: int, ServiceAccountModel
+            groups = account.groups
+            platform: Platforms = kwargs.get('platform')
+            api = await get_api(account.data, platform)
+
+            if not api:
+                raise ValueError('Произошла ошибка при получении объекта API')
+
+            tasks.append(asyncio.create_task(
+                collect_stats(groups, api, platform, **{'Type': kwargs.get('Type')}),
+                name=f'processing-by-{platform.alias}-acc-№{idx + 1}'))
+
+        return tasks, True
+    except ValueError:
+        raise
+
+
+async def create_sending_tasks(stats_results, stats_type):
+    tasks: list[Task] = []
+    for account in stats_results:
+        for group_stats in account:
+            tasks.append(asyncio.create_task(
+                handle_stats(group_stats, stats_type)
+            ))
+
+    return tasks
