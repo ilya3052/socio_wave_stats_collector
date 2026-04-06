55f4a16716e067a742822025a6d52f7b55a9db3e
ilya3052 <ipopadin5@gmail.com>
2026-04-06 22:25:14 +0300

    обработка ошибок и порождение исключений наверх

--- src/handlers/handle_tg.py
+++ src/handlers/handle_tg.py
@@ -1,23 +1,28 @@
 from datetime import datetime
 from typing import Dict
 
+from src.exceptions.exceptions import GroupHandleError
 from src.platformStats.TGStat import TGStat
 
 
 async def handle_tg_group(api, group, **kwargs):
-    try:
-        options = kwargs.get('options')
-        stat = TGStat(api=api, group_id=group.external_id, **options)
-        start = datetime.now()
-        await stat.prepare_object()
-        now = datetime.now()
-        posts_count = await stat.get_service_data()
+    options = kwargs.get('options')
 
-        print(f'Время полной обработки с запросами - {(now - start).total_seconds()} c.\n'
-              f'Обработано записей: {posts_count}')
+    stat = TGStat(api=api, group_id=group.external_id, **options)
 
-        data: Dict[str, str | int] = await stat.get_data()
-        data['Internal ID'] = group.id
-        return data
-    except Exception:
-        raise
+    start = datetime.now()
+    if not await stat.prepare_object():
+        raise GroupHandleError(f'Произошла ошибка при обработке группы {group.name} на платформе TG')
+    now = datetime.now()
+
+    posts_count = await stat.get_service_data()
+
+    print(f'Время полной обработки с запросами - {(now - start).total_seconds()} c.\n'
+          f'Обработано записей: {posts_count}')
+
+    data: Dict[str, str | int] = await stat.get_data()
+    if not data:
+        raise ValueError(f'Произошла ошибка при получении данных для группы {group.name} на платформе TG')
+    data['Internal ID'] = group.id
+    return data
+
--- src/handlers/handle_vk.py
+++ src/handlers/handle_vk.py
@@ -1,19 +1,18 @@
 from datetime import datetime
 from typing import Dict
 
-from src.models import GroupSchema
+from src.exceptions.exceptions import GroupHandleError
 from src.platformStats import VKStat
 
 
-async def handle_vk_group(**kwargs):
-    api = kwargs.get('api')
+async def handle_vk_group(api, group, **kwargs):
     options = kwargs.get('options')
-    group: GroupSchema = kwargs.get('group')
 
     stat = VKStat(api=api, group_id=group.external_id, **options)
 
     start = datetime.now()
-    await stat.prepare_object()
+    if not await stat.prepare_object():
+        raise GroupHandleError(f'Произошла ошибка при обработке группы {group.name} на платформе ВК')
     now = datetime.now()
 
     # будет писать куда-нибудь в логи
@@ -23,5 +22,7 @@ async def handle_vk_group(**kwargs):
           f'Обработано записей: {posts_count}')
 
     data: Dict[str, str | int] = await stat.get_data()
+    if not data:
+        raise ValueError(f'Произошла ошибка при получении данных для группы {group.name} на платформе ВК')
     data['Internal ID'] = group.id
     return data
