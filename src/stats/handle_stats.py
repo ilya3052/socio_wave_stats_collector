129794de65d6c0614c46b13fad35d32e49c7275a
ilya3052 <ipopadin5@gmail.com>
2026-04-06 22:23:15 +0300

    добавление исключени value error для типа снапшота и re-raise validation error выше

--- src/stats/handle_stats.py
+++ src/stats/handle_stats.py
@@ -1,13 +1,20 @@
+from pydantic import ValidationError
+
 from src.core import Type
 from .send import send_daily_stats_to_db, send_absolute_stats_to_db
 
 
 async def handle_stats(stats, stats_type):
-    match stats_type:
-        case Type.DAILY | Type.HOURLY:
-            if not await send_daily_stats_to_db(stats, stats_type):
-                return False
-        case Type.ABSOLUTE:
-            if not await send_absolute_stats_to_db(stats):
-                return False
-    return True
+    try:
+        match stats_type:
+            case Type.DAILY | Type.HOURLY:
+                if not await send_daily_stats_to_db(stats, stats_type):
+                    return False
+            case Type.ABSOLUTE:
+                if not await send_absolute_stats_to_db(stats):
+                    return False
+            case _:
+                raise ValueError('Неизвестный тип снапшота')
+        return True
+    except ValidationError:
+        raise
