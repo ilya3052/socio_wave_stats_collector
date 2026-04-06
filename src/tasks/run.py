3dc3f34baca30bf7c0fc57a812a92a3bfb7a2d86
ilya3052 <ipopadin5@gmail.com>
2026-04-06 22:24:15 +0300

    re-raise исключений выше

--- src/tasks/run.py
+++ src/tasks/run.py
@@ -1,6 +1,9 @@
 import asyncio
 
+from pydantic import ValidationError
+
 from .create import create_processing_tasks, create_sending_tasks
+from ..exceptions.exceptions import GroupsNotFoundError, GroupHandleError
 
 
 async def run_processing_tasks(accounts, **kwargs):
@@ -10,9 +13,18 @@ async def run_processing_tasks(accounts, **kwargs):
         return await tasks_result
     except ValueError:
         raise
+    except GroupsNotFoundError:
+        raise
+    except GroupHandleError:
+        raise
 
 
 async def run_sending_tasks(stats_results, stats_type):
-    tasks = await create_sending_tasks(stats_results, stats_type)
-    tasks_result = asyncio.gather(*tasks)
-    return await tasks_result
+    try:
+        tasks = await create_sending_tasks(stats_results, stats_type)
+        tasks_result = asyncio.gather(*tasks)
+        return await tasks_result
+    except ValidationError:
+        raise
+    except ValueError:
+        raise
