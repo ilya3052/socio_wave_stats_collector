c057e451cc8af3edf59b92b571eaf14de4ae12eb
ilya3052 <ipopadin5@gmail.com>
2026-04-06 15:57:06 +0300

    удаление функции получения групп; изменение функции collect_stats

--- src/stats/collect.py
+++ src/stats/collect.py
@@ -1,23 +1,8 @@
-from typing import Optional, List, Dict
+from typing import List, Dict
 
-from telethon import TelegramClient
-from vk_api.vk_api import VkApiMethod
-
-from src.core import Session, Platforms, get_vk_api_session
+from src.core import Platforms
 from src.handlers import handle_vk_group, handle_tg_group
 from src.models import GroupSchema
-from src.repositories import GroupsRepository
-
-
-async def get_groups(platform_id):
-    groups: Optional[List[GroupSchema]] = None
-
-    with Session() as session:
-        repo = GroupsRepository(session)
-        groups: List[GroupSchema] = [GroupSchema.model_validate(group) for group in
-                                     repo.get_groups_by_platform(platform_id)]
-
-    return groups
 
 
 async def collect_vk_stats(api, groups, **kwargs):
@@ -34,9 +19,7 @@ async def collect_vk_stats(api, groups, **kwargs):
 async def collect_tg_stats(api, groups, **kwargs):
     stats: List[Dict[str, str | int]] = []
     for group in groups:  # type: GroupSchema
-        stats.append(await handle_tg_group(**{
-            "group": group,
-            "api": api,
+        stats.append(await handle_tg_group(api, group, **{
             "options": kwargs
         }))
     return stats
@@ -48,22 +31,16 @@ collect_functions_dict = {
 }
 
 
-async def collect_stats(**kwargs):
-    platform = kwargs.get('platform')
-    api: Optional[VkApiMethod | TelegramClient] = None
-    match platform:
-        case Platforms.VK:
-            api = get_vk_api_session()
-        case Platforms.TG:
-            api = kwargs.pop('api')
-
-    if not api:
-        raise ValueError('Произошла ошибка при получении объекта API')
-
-    groups = await get_groups(platform.id)
-
-    collect_func = collect_functions_dict.get(platform.alias, None)
-    if not collect_func:
-        raise ValueError('Неизвестная платформа')
-    stats = await collect_func(api, groups, **kwargs)
-    return stats
+async def collect_stats(groups, api, platform, **kwargs):
+    try:
+        collect_func = collect_functions_dict.get(platform.alias, None)
+        if not collect_func:
+            raise ValueError('Неизвестная платформа')
+        if platform == Platforms.VK:
+            stats = await collect_func(api, groups, **kwargs)
+        elif platform == Platforms.TG:
+            async with api:
+                stats = await collect_func(api, groups, **kwargs)
+        return stats
+    except ValueError:
+        raise
