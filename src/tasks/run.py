61ecf432f0a258bdc7beaf11cd983021147bebc2
ilya3052 <ipopadin5@gmail.com>
2026-04-06 15:58:54 +0300

    создание функций по запуску задач

---
+++ src/tasks/run.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from .create import create_processing_tasks, create_sending_tasks
+
+
+async def run_processing_tasks(accounts, **kwargs):
+    try:
+        tasks = await create_processing_tasks(accounts, **kwargs)
+        tasks_result = asyncio.gather(*tasks)
+        return await tasks_result, True
+    except ValueError:
+        raise
+
+
+async def run_sending_tasks(stats_results, stats_type):
+    tasks = await create_sending_tasks(stats_results, stats_type)
+    tasks_result = asyncio.gather(*tasks)
+    return await tasks_result
