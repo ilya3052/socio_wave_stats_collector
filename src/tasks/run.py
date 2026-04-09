3cf7eec7708c67e4a84e1ee16b5ce7f3a13b17c6
ilya3052 <ipopadin5@gmail.com>
2026-04-09 22:07:36 +0300

    настройка логирования действий по созданию асинхронных задач отправки данных, обработка исключений и их проброс наверх

--- src/tasks/run.py
+++ src/tasks/run.py
@@ -6,28 +6,32 @@ from sqlalchemy.exc import NoResultFound
 from src.exceptions import GroupsNotFoundError, GroupHandleError
 from .create import create_processing_tasks, create_sending_tasks
 
+import logging
+logger = logging.getLogger(__name__)
 
 async def run_processing_tasks(accounts, **kwargs):
     try:
         tasks = await create_processing_tasks(accounts, **kwargs)
-        tasks_result = asyncio.gather(*tasks)
-        return await tasks_result
-    except ValueError:
+        logger.info(f"Запуск {len(tasks)} задач обработки статистики")
+        return await asyncio.gather(*tasks, return_exceptions=False)
+    except (ValueError, GroupsNotFoundError, GroupHandleError):
         raise
-    except GroupsNotFoundError:
-        raise
-    except GroupHandleError:
+    except Exception:
+        logger.exception("Критическая ошибка при выполнении run_processing_tasks")
         raise
 
 
 async def run_sending_tasks(stats_results, stats_type):
     try:
         tasks = await create_sending_tasks(stats_results, stats_type)
-        tasks_result = asyncio.gather(*tasks)
-        return await tasks_result
+        logger.info(f"Запуск {len(tasks)} задач отправки статистики в БД")
+        return await asyncio.gather(*tasks, return_exceptions=False)
     except ValidationError:
         raise
     except ValueError:
         raise
     except NoResultFound:
+        raise
+    except Exception:
+        logger.exception("Критическая ошибка при выполнении run_sending_tasks")
         raise
