1704645290cb4212a6a890ce95ee7cb048b84699
ilya3052 <ipopadin5@gmail.com>
2026-04-09 22:01:37 +0300

    добавление логирования действий при обработке группы ТГ

--- src/handlers/handle_tg.py
+++ src/handlers/handle_tg.py
@@ -1,8 +1,11 @@
+import logging
 from datetime import datetime
 from typing import Dict
 
 from src.exceptions import GroupHandleError
 from src.platformStats import TGStat
+
+logger = logging.getLogger(__name__)
 
 
 async def handle_tg_group(api, group, **kwargs):
@@ -11,18 +14,22 @@ async def handle_tg_group(api, group, **kwargs):
     stat = TGStat(api=api, group_id=group.external_id, **options)
 
     start = datetime.now()
-    if not await stat.prepare_object():
-        raise GroupHandleError(f'Произошла ошибка при обработке группы {group.name} на платформе TG')
+    try:
+        if not await stat.prepare_object():
+            raise GroupHandleError(f'Не удалось подготовить объект группы {group.name} (TG)')
+    except Exception as e:
+        logger.exception(f"Критическая ошибка обработки группы TG {group.name} с ID {group.external_id}")
+        raise GroupHandleError(f'Произошла ошибка при обработке группы {group.name} на платформе TG') from e
     now = datetime.now()
 
     posts_count = await stat.get_service_data()
 
-    print(f'Время полной обработки с запросами - {(now - start).total_seconds()} c.\n'
-          f'Обработано записей: {posts_count}')
+    logger.info(
+        f"TG группа '{group.name}' обработана за {(now - start).total_seconds():.2f} сек. Обработано записей: {posts_count}",
+    )
 
     data: Dict[str, str | int] = await stat.get_data()
     if not data:
         raise ValueError(f'Произошла ошибка при получении данных для группы {group.name} на платформе TG')
     data['Internal ID'] = group.id
     return data
-
