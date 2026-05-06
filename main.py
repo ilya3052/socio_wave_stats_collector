e483c617968733924bb4b555de162d353d9901b0
ilya3052 <ipopadin5@gmail.com>
2026-05-06 17:45:28 +0300

    добавление функционала для опции --update связанной с обновлением топа постов

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import logging
 import os
 import sys
 
-from sqlalchemy.exc import NoResultFound
 from pydantic import ValidationError
+from sqlalchemy.exc import NoResultFound
+
 from src.core import Platforms
 from src.core import Session, Type
 from src.exceptions import GroupsNotFoundError, GroupHandleError, SendingError
@@ -24,7 +25,9 @@ async def get_serv_accounts(platform_id):
         return repo.get_with_groups_by_platform(platform_id)
 
 
-async def start_collecting_statistics(platform, _type):
+async def start_collecting_statistics(platform, args):
+    print(args)
+    _type = args[0].lstrip('-')
     try:
         logger.info("Запуск сбора статистики")
         logger.info(f"Платформа: {platform} | Тип: {_type}")
@@ -34,8 +37,10 @@ async def start_collecting_statistics(platform, _type):
 
         accounts = await get_serv_accounts(_platform.id)
         logger.info(f"Найдено сервисных аккаунтов: {len(accounts)} (групп: {sum(len(acc.groups) for acc in accounts)})")
-
         options = {'platform': Platforms(platform), 'Type': stats_type}
+
+        if len(args) > 1:
+            options['additional'] = args[1].lstrip('-')
 
         processing_tasks_result = await run_processing_tasks(accounts, **options)
         logger.info("Этап обработки статистики завершён")
@@ -54,17 +59,19 @@ def main():
     ИСПОЛЬЗОВАНИЕ
     main.py <command> [options]
 КОМАНДЫ
-    vk             Сбор статистики групп ВКонтакте
-    tg              Сбор статистики каналов Телеграм
+    vk                      Сбор статистики групп ВКонтакте
+    tg                      Сбор статистики каналов Телеграм
 ОПЦИИ
-    --absolute         Сбор полной статистики группы
-    --daily            Сбор статистики за последние сутки
-    --hourly          Сбор статистики за последние два часа
-    -ct --create-tables   Создание таблиц в базе
-    -h --help             Показать эту подсказку
+    --absolute              Сбор полной статистики группы
+    --daily                 Сбор статистики за последние сутки
+    --hourly                Сбор статистики за последние два часа
+    --update                Обновить топ постов
+    -ct --create-tables     Создание таблиц в базе
+    -h --help               Показать эту подсказку
 ПРИМЕРЫ
-    main.py vkontakte -a
+    main.py vk --absolute
     main.py --create-tables
+    main.py tg --absolute --update
     """
     try:
         if len(sys.argv) == 1 or sys.argv[1].lstrip('-') in ('h', 'help'):
@@ -77,8 +84,9 @@ def main():
             asyncio.run(create_basic_elem())
             logger.info("Таблицы успешно созданы")
         else:
+            print(sys.argv)
             configure_logging(sys.argv[1].lstrip('-'))
-            asyncio.run(start_collecting_statistics(sys.argv[1], sys.argv[2].lstrip('-')))
+            asyncio.run(start_collecting_statistics(sys.argv[1], sys.argv[2:]))
 
     except KeyboardInterrupt:
         logger.warning("Программа прервана пользователем во время выполнения (KeyboardInterrupt)")
