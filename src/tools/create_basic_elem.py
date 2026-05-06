83171a4c04915b202009e206b70a6e752994dab1
ilya3052 <ipopadin5@gmail.com>
2026-05-06 22:42:10 +0300

    добавление новых таблиц и подгонка их под базу данных сгенерированную django orm

--- src/tools/create_basic_elem.py
+++ src/tools/create_basic_elem.py
@@ -28,21 +28,21 @@ async def create_basic_elem():
 
         repo = ServiceAccountRepository(session)
         sacc_vk = ServiceAccountSchema(**{
-            "serviceAccount_id": 1,
+            "service_account_id": 1,
             "serviceAccount_name": 'SocialPulse',
             "serviceAccount_is_activated": True,
             "serviceAccount_app_id": 54438538,
             "platform_id": 1
         })
         sacc_vk2 = ServiceAccountSchema(**{
-            "serviceAccount_id": 2,
+            "service_account_id": 2,
             "serviceAccount_name": 'SocialPulse2',
             "serviceAccount_is_activated": True,
             "serviceAccount_app_id": 54445084,
             "platform_id": 1
         })
         sacc_tg = ServiceAccountSchema(**{
-            "serviceAccount_id": 3,
+            "service_account_id": 3,
             "serviceAccount_name": 'SocialPulse',
             "serviceAccount_is_activated": True,
             "platform_id": 2
@@ -60,7 +60,7 @@ async def create_basic_elem():
             "group_name": "Липецкий Политех (ЛГТУ)",
             "group_link": "https://vk.ru/infolgtu",
             "group_addedAt": datetime.now().date(),
-            "serviceAccount_id": 1,
+            "service_account_id": 1,
             "platform_id": 1
         })
         group_vk2 = GroupSchema(**{
@@ -69,7 +69,7 @@ async def create_basic_elem():
             "group_name": "Кафедра АСУ ЛГТУ - Официальная группа",
             "group_link": "https://vk.ru/asu.lgtu",
             "group_addedAt": datetime.now().date(),
-            "serviceAccount_id": 2,
+            "service_account_id": 2,
             "platform_id": 1
         })
         group_vk3 = GroupSchema(**{
@@ -78,7 +78,7 @@ async def create_basic_elem():
             "group_name": "Аниме магазин Maneki (Липецк)",
             "group_link": "https://vk.ru/maneki48",
             "group_addedAt": datetime.now().date(),
-            "serviceAccount_id": 2,
+            "service_account_id": 2,
             "platform_id": 1
         })
         group_tg = GroupSchema(**{
@@ -87,7 +87,7 @@ async def create_basic_elem():
             "group_name": "Липецкий Политех (ЛГТУ)",
             "group_link": "https://t.me/infolgtu",
             "group_addedAt": datetime.now().date(),
-            "serviceAccount_id": 3,
+            "service_account_id": 3,
             "platform_id": 2
         })
 
@@ -109,19 +109,19 @@ async def create_basic_elem():
             "serviceAccountData_id": 1,
             "serviceAccountData_serviceKey": 'b37b1bceb37b1bceb37b1bce66b045b144bb37bb37b1bceda04751ed922c3191f19eaad',
             "serviceAccountData_protectedKey": 'chq1b4w5YPy71LuKFX70',
-            "serviceAccount_id": 1
+            "service_account_id": 1
         })
         data_vk2 = ServiceAccountDataSchema(**{
             "serviceAccountData_id": 2,
             "serviceAccountData_serviceKey": '236bc685236bc685236bc6856c205502992236b236bc6854aec6189447cb332c4bbfd8b',
             "serviceAccountData_protectedKey": 'LvuSPkzu3Yc61C6vvVxe',
-            "serviceAccount_id": 2
+            "service_account_id": 2
         })
         data_tg = ServiceAccountDataSchema(**{
             "serviceAccountData_id": 3,
             "serviceAccountData_phoneNumber": '79205189704',
             "serviceAccountData_sessionPath": 'sessions/79205189704.session',
-            "serviceAccount_id": 3,
+            "service_account_id": 3,
         })
 
         repo.add(ServiceAccountDataModel(**data_vk.model_dump()))
